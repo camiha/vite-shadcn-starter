396190cc66b6570b525e830b57d5adafbac0bd3a
Kamihara <hello.camiha@gmail.com>
2023-12-23 15:17:07 +0900

    set typography component for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { TypographyH1, TypographyP } from "@/components/typography";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -8,17 +9,15 @@ function App() {
 	return (
 		<div>
 			<div className="h-screen w-screen flex flex-col justify-center items-center gap-8">
-				<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-					Vite + shadcn/ui
-				</h1>
+				<TypographyH1>Vite + shadcn/ui</TypographyH1>
 				<div className="flex flex-col gap-2">
 					<Button onClick={() => setCount((count) => count + 1)}>
 						{" "}
 						count is {count}
 					</Button>
-					<p className="leading-7">
+					<TypographyP>
 						Edit <code>src/App.tsx</code> and save to test HMR
-					</p>
+					</TypographyP>
 				</div>
 				<div className="fixed top-8 right-8">
 					<ModeToggle />
