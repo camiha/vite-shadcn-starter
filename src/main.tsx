3e3df984453d7817230e5b75ac30561622879eae
Kamihara <hello.camiha@gmail.com>
2023-12-23 14:22:19 +0900

    fix linting errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// biome-ignore lint/style/noNonNullAssertion: root element is always present in html
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
